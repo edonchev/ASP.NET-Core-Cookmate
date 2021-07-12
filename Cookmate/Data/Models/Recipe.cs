db0b64fe0e37251d6776aa71bf5b1524df30ccab
Encho Donchev <32332495+edonchev@users.noreply.github.com>
2021-07-12 16:38:13 +0300

    A few models added

---
+++ Cookmate/Data/Models/Recipe.cs
@@ -0,0 +1,29 @@
+﻿namespace Cookmate.Data.Models
+{
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+
+    public class Recipe
+    {
+        [Required]
+        public int Id { get; init; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public int CookingTime { get; set; }
+
+        public int Likes { get; set; }
+
+        public string PictureUrl { get; set; }
+
+        public int CategoryId { get; set; }
+
+        public Category Category { get; init; }
+
+        [Required]
+        public IEnumerable<Ingredient> Ingredients { get; set; } = new List<Ingredient>();
+    }
+}
